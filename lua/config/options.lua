47b604e6be5be7d3fab089bf04f0db3def7ca99b
A. Balmaseda <aitor.5502.b@gmail.com>
2024-08-07 18:05:54 +0200

    Changes for deskarch

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,18 +2,3 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.maplocalleader = "ñ"
-
-if vim.fn.has("wsl") then
-  vim.g.clipboard = {
-    name = "WslClipboard",
-    copy = {
-      ["+"] = "win32yank.exe -i",
-      ["*"] = "win32yank.exe -i",
-    },
-    paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf",
-    },
-    cache_enabled = true,
-  }
-end
