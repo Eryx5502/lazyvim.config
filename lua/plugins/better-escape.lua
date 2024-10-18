88cef9f012fc9b44ff7e3f422ded393b8f9d7314
A. Balmaseda <aitor.5502.b@gmail.com>
2024-10-18 12:17:07 +0200

    Better-escape rework, sioyek for wsl

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -1,8 +1,6 @@
 return {
   "max397574/better-escape.nvim",
   config = function()
-    require("better_escape").setup({
-      mapping = { "kj", "jk" },
-    })
+    require("better_escape").setup()
   end,
 }
--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -8,10 +8,21 @@ return {
       vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
       vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
       vim.g.tex_flavor = "latex"
-      vim.g.vimtex_view_method = "zathura"
+      if
+        vim.fn.has("win32") or (vim.fn.has("unix") and vim.fn.exists("$WSLENV")) and vim.fn.executable("sioyek.exe")
+      then
+        vim.g.vimtex_view_method = "sioyek"
+        vim.g.vimtex_view_sioyek_exe = "sioyek.exe"
+        vim.g.vimtex_callback_progpath = "wsl -e bash -lic nvim"
+      else
+        vim.g.vimtex_view_method = "zathura"
+      end
       vim.g.vimtex_quickfix_mode = 0
       vim.g.vimtex_imaps_enabled = 0
     end,
+    cmd = {
+      "VimtexInverseSearch",
+    },
   },
   {
     "neovim/nvim-lspconfig",
